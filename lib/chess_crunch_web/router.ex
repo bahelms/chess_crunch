ad9383d94d050dd9e7372b2591a6b8796fb3c474
bahelms <jimbonkgit@gmail.com>
2021-04-13 08:25:04 -0400

    Login form styling

--- lib/chess_crunch_web/router.ex
+++ lib/chess_crunch_web/router.ex
@@ -15,8 +15,6 @@ defmodule ChessCrunchWeb.Router do
 
   scope "/", ChessCrunchWeb do
     pipe_through :browser
-
-    live "/", PageLive, :index
   end
 
   # Enables LiveDashboard only for development
@@ -53,6 +51,7 @@ defmodule ChessCrunchWeb.Router do
   scope "/", ChessCrunchWeb do
     pipe_through [:browser, :require_authenticated_user]
 
+    live "/", PageLive, :index
     get "/users/settings", UserSettingsController, :edit
     put "/users/settings", UserSettingsController, :update
     get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
