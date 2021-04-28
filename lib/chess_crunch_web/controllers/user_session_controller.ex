e32e8197adc3b474eebf232b61550e6faaff50c0
bahelms <jimbonkgit@gmail.com>
2021-04-27 21:52:49 -0400

    Various things

--- lib/chess_crunch_web/controllers/user_session_controller.ex
+++ lib/chess_crunch_web/controllers/user_session_controller.ex
@@ -20,7 +20,7 @@ defmodule ChessCrunchWeb.UserSessionController do
 
   def delete(conn, _params) do
     conn
-    |> put_flash(:info, "Logged out successfully.")
+    |> put_flash(:info, "Logged out successfully")
     |> UserAuth.log_out_user()
   end
 end
