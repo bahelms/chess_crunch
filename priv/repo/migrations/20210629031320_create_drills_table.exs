76385333d96851027145a6b81e3f25a5c025c8a4
bahelms <jimbonkgit@gmail.com>
2021-07-20 23:29:23 -0400

    Removes drill field

--- priv/repo/migrations/20210629031320_create_drills_table.exs
+++ priv/repo/migrations/20210629031320_create_drills_table.exs
@@ -6,7 +6,6 @@ defmodule ChessCrunch.Repo.Migrations.CreateDrillsTable do
       add :id, :binary_id, primary_key: true
       add :answer, :text
       add :duration, :integer
-      add :completed_on, :utc_datetime
       add :cycle_id, references(:cycles, on_delete: :delete_all)
       add :position_id, references(:positions, type: :binary_id, on_delete: :delete_all)
       timestamps()
