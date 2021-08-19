9b3b74b999ae07dd36d43650d6f29c646ef41940
bahelms <jimbonkgit@gmail.com>
2021-08-19 09:23:23 -0400

    Get drills with solutions working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,23 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import '../css/app.scss'
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
 // in "webpack.config.js".
-//
-// Import deps with the dep name or local files with a relative path, for example:
-//
-//     import {Socket} from "phoenix"
-//     import socket from "./socket"
-//
-import "phoenix_html"
-import { Socket } from "phoenix"
-import topbar from "topbar"
-import { LiveSocket } from "phoenix_live_view"
-import Alpine from "alpinejs"
-import { objToFen } from "chessboard-element"
-import Chess from "chess.js"
+import 'phoenix_html'
+import { Socket } from 'phoenix'
+import topbar from 'topbar'
+import { LiveSocket } from 'phoenix_live_view'
+import Alpine from 'alpinejs'
+import { objToFen } from 'chessboard-element'
+import Chess from 'chess.js'
 
 const liveViewHooks = {
   chessboard: {
     game: null,
+
     startGame() {
       const fen = this.el.getAttribute('fen')
       this.game = new Chess(fen)
@@ -31,18 +26,20 @@ const liveViewHooks = {
     mounted() {
       this.startGame()
 
-      this.el.addEventListener("drag-start", (e) => {
+      this.el.addEventListener('drag-start', (e) => {
         const { source, piece, position, orientation } = e.detail
 
         // only pick up pieces for the side to move
-        if ((this.game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-            (this.game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+        if (
+          (this.game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+          (this.game.turn() === 'b' && piece.search(/^w/) !== -1)
+        ) {
           e.preventDefault()
           return
         }
       })
 
-      this.el.addEventListener("drop", (e) => {
+      this.el.addEventListener('drop', (e) => {
         const { source, target, setAction } = e.detail
         const move = this.game.move({ from: source, to: target })
         const duration = this.el.getAttribute('duration')
@@ -50,41 +47,49 @@ const liveViewHooks = {
 
         // prevent illegal moves
         if (move === null) {
-          return setAction("snapback")
+          return setAction('snapback')
         }
-        this.pushEvent(event, { pgn: this.game.pgn(), fen: this.game.fen(), duration })
+        this.pushEvent(event, {
+          pgn: this.game.pgn(),
+          fen: this.game.fen(),
+          duration,
+        })
       })
 
-      this.el.addEventListener("snap-end", (e) => {
+      this.el.addEventListener('snap-end', (e) => {
         // update board for special cases: castling, en passant, pawn promotion
         this.el.setPosition(this.game.fen())
       })
 
-      this.handleEvent("stop_drill", (payload) => {
-        document.dispatchEvent(new CustomEvent("stop_drill", {detail: payload}))
+      this.handleEvent('stop_drill', () => {
+        document.dispatchEvent(new Event('stop_drill'))
       })
-    }
-  }
+
+      this.handleEvent('new_game', ({fen}) => {
+        this.game = new Chess(fen)
+      })
+    },
+  },
 }
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute('content')
+let liveSocket = new LiveSocket('/live', Socket, {
   hooks: liveViewHooks,
-  params: {_csrf_token: csrfToken},
+  params: { _csrf_token: csrfToken },
   // make LiveView work nicely with alpinejs
   dom: {
     onBeforeElUpdated(from, to) {
       if (from._x_dataStack) {
         Alpine.clone(from, to)
       }
-    }
-  }
+    },
+  },
 })
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+topbar.config({ barColors: { 0: '#29d' }, shadowColor: 'rgba(0, 0, 0, .3)' })
+window.addEventListener('phx:page-loading-start', (info) => topbar.show())
+window.addEventListener('phx:page-loading-stop', (info) => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
@@ -96,29 +101,26 @@ liveSocket.connect()
 window.liveSocket = liveSocket
 
 const zeroPad = (val) => {
-  const valString = val + ""
+  const valString = val + ''
   if (valString.length == 2) {
     return valString
   }
-  return "0" + valString
+  return '0' + valString
 }
 
 window.objToFen = objToFen
 
-document.addEventListener("alpine:init", () => {
-  Alpine.data("drill", () => ({
+document.addEventListener('alpine:init', () => {
+  Alpine.data('drill', () => ({
     seconds: 0,
-    answer: "",
     timer: null,
     draggablePieces: true,
 
     init() {
       this.timer = setInterval(() => this.seconds++, 1000)
-      document.addEventListener("stop_drill", ({detail: {status}}) => {
-        if (status === "incorrect") {
-          clearInterval(this.timer)
-          this.draggablePieces = false
-        }
+      document.addEventListener('stop_drill', () => {
+        clearInterval(this.timer)
+        this.draggablePieces = false
       })
     },
 
--- lib/chess_crunch_web/live/cycle_live.ex
+++ lib/chess_crunch_web/live/cycle_live.ex
@@ -5,20 +5,50 @@ defmodule ChessCrunchWeb.CycleLive do
   defdelegate format_to_play(position, opts), to: Sets
   defdelegate format_to_play(position), to: Sets
 
+  defp initial_state(drill), do: [drill: drill, fen: drill.position.fen, status: nil]
+
   @impl true
   def mount(%{"id" => cycle_id}, _session, socket) do
     round_id = Cycles.current_round_for_cycle(cycle_id).id
     drill = Cycles.next_drill(round_id)
+    {:ok, assign(socket, initial_state(drill))}
+  end
 
-    {:ok,
-     assign(socket,
-       drill: drill,
-       round_id: round_id,
-       fen: drill.position.fen,
-       duration: nil,
-       answer: nil,
-       status: nil
-     )}
+  @impl true
+  def handle_event(
+        "board_update",
+        %{"pgn" => pgn, "fen" => fen, "duration" => duration},
+        %{assigns: %{drill: drill}} = socket
+      ) do
+    case Drills.evaluate_moves(drill.position.solution_moves, PGN.new(pgn).moves) do
+      {:correct, _moves} ->
+        {:noreply, assign(socket, %{fen: fen})}
+
+      {:full_match, moves} ->
+        drill = Drills.create_drill(drill, %{answer: moves, duration: duration})
+        {:noreply, stop_drill(socket, drill, fen, :completed)}
+
+      {:incorrect, moves} ->
+        drill = Drills.create_drill(drill, %{answer: moves, duration: duration})
+        {:noreply, stop_drill(socket, drill, fen, :incorrect)}
+    end
+  end
+
+  @impl true
+  def handle_event("next_drill", _, %{assigns: assigns} = socket) do
+    case Cycles.complete_drill(assigns[:drill]) do
+      {:next_drill, drill} ->
+        {:noreply, new_drill(socket, drill)}
+
+      _ ->
+        # TODO: show that the round needs to be repeated due to low accuracy
+        socket =
+          socket
+          |> put_flash(:info, "Round completed!")
+          |> redirect(to: Routes.cycle_path(socket, :index))
+
+        {:noreply, socket}
+    end
   end
 
   @impl true
@@ -30,7 +60,7 @@ defmodule ChessCrunchWeb.CycleLive do
 
     case Cycles.complete_drill(assigns[:drill]) do
       {:next_drill, drill} ->
-        {:noreply, assign(socket, drill: drill, status: nil)}
+        {:noreply, assign(socket, initial_state(drill))}
 
       _ ->
         socket =
@@ -42,38 +72,15 @@ defmodule ChessCrunchWeb.CycleLive do
     end
   end
 
-  @impl true
-  def handle_event(
-        "board_update",
-        %{"pgn" => pgn, "fen" => fen, "duration" => duration},
-        %{assigns: %{drill: drill}} = socket
-      ) do
-    case Drills.evaluate_moves(drill.position.solution_moves, PGN.new(pgn).moves) do
-      {:correct, moves} ->
-        # rerender so that next move can be played
-        {:noreply, assign(socket, %{answer: moves, fen: fen, duration: duration})}
+  defp stop_drill(socket, drill, fen, status) do
+    socket
+    |> assign(%{drill: drill, fen: fen, status: status})
+    |> push_event("stop_drill", %{})
+  end
 
-      {:full_match, moves} ->
-        # move is correct AND drill is complete
-        # Show correct in view and next drill button
-        # Stop timer
-        {:noreply, assign(socket, %{answer: moves, fen: fen, duration: duration})}
-
-      {:incorrect, moves} ->
-        drill = Drills.create_drill(drill, %{answer: moves, duration: duration})
-
-        socket =
-          socket
-          |> assign(%{
-            drill: drill,
-            answer: moves,
-            fen: fen,
-            duration: duration,
-            status: :incorrect
-          })
-          |> push_event("stop_drill", %{status: :incorrect})
-
-        {:noreply, socket}
-    end
+  defp new_drill(socket, drill) do
+    socket
+    |> assign(initial_state(drill))
+    |> push_event("new_game", %{fen: drill.position.fen})
   end
 end
