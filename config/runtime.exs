5793549585b08d7b5e7981832a28f0e4e95c385d
bahelms <jimbonkgit@gmail.com>
2021-09-24 15:49:15 -0400

    Get config working in prod and dev

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,29 +1,19 @@
 import Config
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+if config_env() == :prod do
+  port = System.get_env("PORT", "80")
 
-config :chess_crunch, ChessCrunchWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
-  secret_key_base: secret_key_base,
-  server: true
+  config :chess_crunch, ChessCrunchWeb.Endpoint,
+    http: [
+      port: String.to_integer(port),
+      transport_options: [socket_opts: [:inet6]]
+    ],
+    url: [host: System.get_env("HOST"), port: port],
+    secret_key_base: System.fetch_env!("SECRET_KEY_BASE"),
+    server: true
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
-
-config :chess_crunch, ChessCrunch.Repo,
-  # ssl: true,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  config :chess_crunch, ChessCrunch.Repo,
+    ssl: true,
+    url: System.fetch_env!("DATABASE_URL"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
+end
